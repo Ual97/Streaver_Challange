4cd8d4384561472e500de6521183433ce1e20d11
Ual97 <3951@holberstonschool.com>
2024-03-05 20:43:39 -0300

    updated post type

--- streaver-challange/src/app/posts.tsx
+++ streaver-challange/src/app/posts.tsx
@@ -1,10 +1,11 @@
 // pages/posts.tsx
 import useSWR from 'swr';
+import { Post } from './types';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Posts = () => {
-  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
+  const { data, error } = useSWR<Post[]>('https://jsonplaceholder.typicode.com/posts', fetcher);
 
   if (error) return <div>Error loading posts</div>;
   if (!data) return <div>Loading...</div>;
@@ -12,7 +13,7 @@ const Posts = () => {
   return (
     <div>
       <h1>Posts</h1>
-      {data.map((post) => (
+      {data.map((post: Post) => (
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.body}</p>
---
+++ streaver-challange/src/app/types.ts
@@ -0,0 +1,9 @@
+// Create a file types.ts and define the interface
+// types.ts
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }
+  
